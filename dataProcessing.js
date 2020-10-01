82ccd8e2e6a094ccc4cadba9fc6a24112d4e983a
dabibu153 <68466028+dabibu153@users.noreply.github.com>
2020-10-01 18:25:39 +0530

    updated logic for altered data flow

--- dataProcessing.js
+++ dataProcessing.js
@@ -25,26 +25,20 @@ router.post("/getDetails", async (req, res) => {
   res.send(wordDetails);
 });
 
-router.post("/searchOxford", async (req, res) => {
-  const rootForm = await fetch(
-    `https://od-api.oxforddictionaries.com/api/v2/lemmas/en-us/${req.body.searchKeyWord}`,
-    {
-      method: "GET",
-      headers: {
-        app_id: "fd907da5",
-        app_key: "5904376a3306a62c2c1d968070b50f8e",
-      },
-    }
-  );
-  const jsonRootForm = await rootForm.json();
-  if (jsonRootForm.error) {
-    res.send("word not found");
+router.post("/searchMongo", async (req, res) => {
+  const wordDetails = await oneWord.findOne({ name: req.body.searchKeyWord });
+  if (wordDetails) {
+    res.send(wordDetails);
   } else {
-    const finalRoot = await jsonRootForm.results[0].lexicalEntries[0]
-      .inflectionOf[0].id;
+    res.send("word not found");
+  }
+});
 
-    const apiResult = await fetch(
-      `https://od-api.oxforddictionaries.com/api/v2/entries/en-us/${finalRoot}?fields=etymologies,definitions,examples`,
+router.post("/searchOxford", async (req, res) => {
+  console.log(req.body.word);
+  try {
+    const rootForm = await fetch(
+      `https://od-api.oxforddictionaries.com/api/v2/lemmas/en-us/${req.body.word}`,
       {
         method: "GET",
         headers: {
@@ -53,69 +47,90 @@ router.post("/searchOxford", async (req, res) => {
         },
       }
     );
-
-    const jsonData = await apiResult.json();
-    let origin = "nil";
-    if (jsonData.results[0].lexicalEntries[0].entries[0].etymologies) {
-      origin = jsonData.results[0].lexicalEntries[0].entries[0].etymologies[0];
+    const jsonRootForm = await rootForm.json();
+    if (jsonRootForm.error) {
+      res.send("word not found");
     } else {
-      origin = "nil";
-    }
+      const finalRoot = await jsonRootForm.results[0].lexicalEntries[0]
+        .inflectionOf[0].id;
 
-    let category = [];
-
-    jsonData.results[0].lexicalEntries.map((categoryType) => {
-      let defAndEg = [];
-
-      categoryType.entries[0].senses.map((defEg) => {
-        let example = "nil";
-        if (defEg.examples) {
-          example = defEg.examples[0].text;
-        } else {
-          example = "nil";
+      const apiResult = await fetch(
+        `https://od-api.oxforddictionaries.com/api/v2/entries/en-us/${finalRoot}?fields=etymologies,definitions,examples`,
+        {
+          method: "GET",
+          headers: {
+            app_id: "fd907da5",
+            app_key: "5904376a3306a62c2c1d968070b50f8e",
+          },
         }
+      );
 
-        const tempObj = {
-          def: defEg.definitions[0],
-          eg: example,
+      const jsonData = await apiResult.json();
+      let origin = "nil";
+      if (jsonData.results[0].lexicalEntries[0].entries[0].etymologies) {
+        origin =
+          jsonData.results[0].lexicalEntries[0].entries[0].etymologies[0];
+      } else {
+        origin = "nil";
+      }
+
+      let category = [];
+
+      jsonData.results[0].lexicalEntries.map((categoryType) => {
+        let defAndEg = [];
+
+        categoryType.entries[0].senses.map((defEg) => {
+          let example = "nil";
+          if (defEg.examples) {
+            example = defEg.examples[0].text;
+          } else {
+            example = "nil";
+          }
+
+          const tempObj = {
+            def: defEg.definitions[0],
+            eg: example,
+          };
+          defAndEg.push(tempObj);
+        });
+
+        categoryObject = {
+          type: categoryType.lexicalCategory.id,
+          defAndEg: defAndEg,
         };
-        defAndEg.push(tempObj);
+
+        category.push(categoryObject);
       });
 
-      categoryObject = {
-        type: categoryType.lexicalCategory.id,
-        defAndEg: defAndEg,
+      const finalWordObject = {
+        name: jsonData.id,
+        origin: origin,
+        category: category,
       };
 
-      category.push(categoryObject);
-    });
+      const oldWord = await oneWord.findOne({ name: finalWordObject.name });
 
-    const finalWordObject = {
-      name: jsonData.id,
-      origin: origin,
-      category: category,
-    };
-
-    const oldWord = await oneWord.findOne({ name: finalWordObject.name });
-
-    if (oldWord) {
-      const result1 = await oneWord.deleteOne({ name: finalWordObject.name });
-      const newWord = new oneWord({
-        name: finalWordObject.name,
-        origin: finalWordObject.origin,
-        category: finalWordObject.category,
-      });
-      await newWord.save();
-      res.send(finalWordObject);
-    } else {
-      const newWord = new oneWord({
-        name: finalWordObject.name,
-        origin: finalWordObject.origin,
-        category: finalWordObject.category,
-      });
-      await newWord.save();
-      res.send(finalWordObject);
+      if (oldWord) {
+        const result1 = await oneWord.deleteOne({ name: finalWordObject.name });
+        const newWord = new oneWord({
+          name: finalWordObject.name,
+          origin: finalWordObject.origin,
+          category: finalWordObject.category,
+        });
+        await newWord.save();
+        res.send(finalWordObject);
+      } else {
+        const newWord = new oneWord({
+          name: finalWordObject.name,
+          origin: finalWordObject.origin,
+          category: finalWordObject.category,
+        });
+        await newWord.save();
+        res.send(finalWordObject);
+      }
     }
+  } catch (error) {
+    res.send("word not found");
   }
 });
 
